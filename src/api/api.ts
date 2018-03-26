8c0306fa10101f9175acec4c56cd1cbf77990bc6
阿林 <724159997@qq.com>
2018-03-27 00:35:12 +0800

    update

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,4 +2,4 @@ export const baseUrl = ''
 export const getList = `${baseUrl}/getMsgList`
 export const notifyMsgResult = `${baseUrl}/setMsgState`
 
-export const testApi = 'http://api.gogold.hk/usercenter/LoginHandler.ashx?action=GetNews'
+export const testApi = ''
